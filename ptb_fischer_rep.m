35c2b71dc91de9aadd5fa546f1e556968e996d60
DrColling <lincoln@colling.net.nz>
2017-01-25 14:21:06 +0000

    "update code to include the aMAS questionnaire"

--- ptb_fischer_rep.m
+++ ptb_fischer_rep.m
@@ -1,5 +1,5 @@
 function PTB_Fischer()
-codeVersion = '0.9.9';
+codeVersion = '1.0';
 
 % --- DO NOT CHANGE -- %
 skipVercheck = 0; % 1 = check matlab and octave versions
@@ -105,7 +105,7 @@ try
         params = A.params;
         disp('loading the parameters file...');
     else
-        error('Params.mat not found. Please run the calibration script (Calibrate.m)')
+        error('Params.mat not found. Please run the calibration script (ptb_fischer_calibration.m)')
     end
     
     %----------------------------------------------------------------------
@@ -634,6 +634,9 @@ try
     
     [mathTestScore, mathTestReponse] = math_test;
     
+    
+    aMAS = GetAMAS;
+    
     handed = getHandedness();
     ListenChar(0); %makes it so characters typed do show up in the command window
     
@@ -655,7 +658,7 @@ try
     
     
     save(subjectDataFile,'responseStruct','trialStruct','timings','params','handed','lang','age','systemParams',...
-        'mathTestScore','mathTestReponse','-mat');
+        'mathTestScore','mathTestReponse','aMAS','-mat');
 catch ME
     sca;
     ListenChar(0); %makes it so characters typed do show up in the command window
@@ -968,21 +971,21 @@ end
             '17 - 9';...
             '89 - 18';...
             '5 x 3';...
-            '8 Ã· 2';...
+            '8 ÷ 2';...
             '8 x 5';...
             '13 x 7';...
             '48 - 19';...
             '14 x 6';...
             '2/3 - 1/3';...
-            '126 Ã· 42';...
-            '288 Ã· 48';...
+            '126 ÷ 42';...
+            '288 ÷ 48';...
             '7/8 - 2/8';...
             '3250 / 25';...
             '2 3/4 + 4 1/8';...
             '1.05 x 0.2';...
             '-18 + 12';...
             '-6 x 7';...
-            '4/7 Ã· 1/2'};
+            '4/7 ÷ 1/2'};
         
         level = reshape(ones(5,5) .* repmat([1 2 3 4 5],5,1),1,25);
         incorrectByLevel = zeros(5,1);
@@ -1249,6 +1252,169 @@ end
         
         
     end
+
+
+function aMAS = GetAMAS
+        
+       
+        questions ={'Having to use the tables in the back of a math book.';...
+            'Thinking about an upcoming math test 1 day before.';...
+            'Watching a teacher work an algebraic equation on the blackboard.';...
+            'Taking an examination in a math course.';...
+            'Being given a homework assignment of many difficult problems that is due the next class meeting.';...
+            'Listening to a lecture in math class.';...
+            'Listening to another student explain a math formula.';...
+            'Being given a "pop" quiz in math class.';...
+            'Starting a new chapter in a math book.'};
+        
+        
+        
+        
+        
+        Screen('TextSize',window,24);
+        DrawFormattedText(window, ['Please rate each item below in terms of how anxious you would feel during the event specified. Press [SPACE] to start.'], 'center', 'center', [1 1 1], 65,[],[],2);
+        Screen('Flip', window);
+        presstogo
+        
+        
+        
+        
+        % Make a base Rect of 200 by 200 pixels
+        baseRect = [0 0 100 100];
+        
+        % Screen X positions of our three rectangles
+        squareXpos = [screenXpixels * 0.30 screenXpixels * 0.40 screenXpixels * 0.50 screenXpixels * 0.60 screenXpixels * 0.70];
+        numSqaures = length(squareXpos);
+        
+        % Set the colors to Red, Green and Blue
+        
+        % Make our rectangle coordinates
+        allRects = nan(4, numSqaures);
+        for i = 1:numSqaures
+            allRects(:, i) = CenterRectOnPointd(baseRect, squareXpos(i), yCenter);
+            if i == 1 || i == 5
+                allRects(:, i) = CenterRectOnPointd(baseRect .* [0 0 2.2 1], squareXpos(i), yCenter);
+            end
+            
+        end
+        
+        allColors = [1 0 0; 0 1 0; 0 0 1];
+        
+        % Define red and blue
+        offSquare = [1 1 1]/10;
+        onSquare = [1 1 1]/2;
+        
+        % Here we set the initial position of the mouse to be in the centre of the
+        % screen
+        SetMouse(xCenter, yCenter, window);
+        
+        % Sync us and get a time stamp
+        vbl = Screen('Flip', window);
+        waitframes = 1;
+        
+        % Maximum priority level
+        topPriorityLevel = MaxPriority(window);
+        Priority(topPriorityLevel);
+        
+        % Loop the animation until a key is pressed
+        buttons = 0;
+        Screen('TextFont', window, 'Ariel');
+        Screen('TextSize', window, 30);
+        [~, ~, textbound1] = DrawFormattedText(window, '1 (low anxiety)', sum(allRects([1 3],1))/2, sum(allRects([2 4],1))/2 , white);
+        [~, ~, textbound2] = DrawFormattedText(window, '2', sum(allRects([1 3],1))/2, sum(allRects([2 4],1))/2 , white);
+        [~, ~, textbound3] = DrawFormattedText(window, '3', sum(allRects([1 3],1))/2, sum(allRects([2 4],1))/2 , white);
+        [~, ~, textbound4] = DrawFormattedText(window, '4', sum(allRects([1 3],1))/2, sum(allRects([2 4],1))/2 , white);
+        [~, ~, textbound5] = DrawFormattedText(window, '5 (high anxiety)', sum(allRects([1 3],1))/2, sum(allRects([2 4],1))/2 , white);
+        
+        q = 1;
+        buttons = 0;
+        left = 0;
+        centre = 0;
+        right = 0;
+        selected = 0;
+        while q<=length(questions)
+            
+            clear buttons
+            % Get the current position of the mouse
+            [x, y, buttons] = GetMouse(window);
+            
+            % Center the rectangle on the centre of the screen
+            
+            % See if the mouse cursor is inside the square
+            one = IsInRect(x, y, allRects(:,1));
+            two = IsInRect(x, y, allRects(:,2));
+            three = IsInRect(x, y, allRects(:,3));
+            four = IsInRect(x, y, allRects(:,4));
+            five = IsInRect(x, y, allRects(:,5));
+            
+            if one == 1
+                allColors(:,1) = onSquare;
+            else
+                allColors(:,1) = offSquare;
+            end
+            
+            if two == 1
+                allColors(:,2) = onSquare;
+            else
+                allColors(:,2) = offSquare;
+            end
+            
+            if three == 1
+                allColors(:,3) = onSquare;
+            else
+                allColors(:,3) = offSquare;
+            end
+            
+            if four == 1
+                allColors(:,4) = onSquare;
+            else
+                allColors(:,4) = offSquare;
+            end
+            
+            
+            if five == 1
+                allColors(:,5) = onSquare;
+            else
+                allColors(:,5) = offSquare;
+            end
+            
+            
+            Screen('FillRect', window, allColors, allRects)
+            
+            
+            DrawFormattedText(window, questions{q}, 'center', yCenter-300, [1 1 1], 70, [], [], 2);
+            
+            
+            DrawFormattedText(window, '1 (low anxiety)', (sum(allRects([1 3],1))/2) - diff(textbound1([1,3]))/2, (sum(allRects([2 4],1))/2) + (diff(textbound1([2,4]))/4) , white);
+            DrawFormattedText(window, '2', (sum(allRects([1 3],2))/2) - diff(textbound2([1,3]))/2, (sum(allRects([2 4],2))/2) + (diff(textbound2([2,4]))/4) , white);
+            DrawFormattedText(window, '3', (sum(allRects([1 3],3))/2) - diff(textbound3([1,3]))/2, (sum(allRects([2 4],3))/2) + (diff(textbound3([2,4]))/4) , white);
+            DrawFormattedText(window, '4', (sum(allRects([1 3],4))/2) - diff(textbound4([1,3]))/2, (sum(allRects([2 4],4))/2) + (diff(textbound4([2,4]))/4) , white);
+            DrawFormattedText(window, '5 (high anxiety)', (sum(allRects([1 3],5))/2) - diff(textbound5([1,3]))/2, (sum(allRects([2 4],5))/2) + (diff(textbound5([2,4]))/4) , white);
+
+            % Draw a white dot where the mouse is
+            Screen('DrawDots', window, [x y], 10, white, [], 2);
+            
+            % Flip to the screen
+            vbl  = Screen('Flip', window, vbl + (waitframes - 0.5) * ifi);
+            
+            if any(buttons) == 1 && (one ==1||two==1||three==1||four==1||five==1)
+                aMAS(q,:) = [(one * 1) + (two * 2) + (three * 3) +  (four * 4) + (five * 5)];
+                q = q + 1;
+                left = 0;
+                right = 0;
+                centre= 0;
+                clear buttons
+                WaitSecs(.2);
+            end
+            
+            
+        end
+        
+        
+    end
+
+
+
 end
 
 
